4826c7898dd0dedaab3cdd26721e59a30f8035a5
Caspek <ckolsrud1@gmail.com>
2025-03-11 15:02:54 +0100

    Ny klasse DeckOfCards Ny field for suit - for å determinere kortets suit Ny metode dealHand som gir et tall mellom 1 og 13 tror jeg Oppdatert pom.xml til å kjøre Main klassen når det blir utført mvn javafx:run i terminalen

---
+++ src/main/java/DeckOfCards.java
@@ -0,0 +1,11 @@
+public class DeckOfCards {
+    private final char[] suit = {'S','H','D','C'};
+
+    static void dealHand(int n){
+        n = (int)(Math.random() * 14);
+    }
+
+    // Metode for å trekke et kort med suit
+
+    // Metode for å sjekke for flush
+}
