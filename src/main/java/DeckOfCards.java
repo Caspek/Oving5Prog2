314f97d7cc16036d2c6c5d43abc2e718cee36b60
Caspek <ckolsrud1@gmail.com>
2025-03-20 12:41:45 +0100

    Did everything last minute

--- src/main/java/DeckOfCards.java
+++ src/main/java/DeckOfCards.java
@@ -1,19 +1,64 @@
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 public class DeckOfCards {
     private static final char[] suits = {'S', 'H', 'D', 'C'};
-    private static final String[] value = {"1", "2", "3", "4", "5", "6", "7",
+    private static final String[] rank = {"1", "2", "3", "4", "5", "6", "7",
             "8", "9", "10", "11", "12", "13"};
 
     public static String[] dealHand(int n) {
         String[] hand = new String[n];
+        Set<String> dealtCards = new HashSet<>();
         Random random = new Random();
 
         for (int i = 0; i < n; i++) {
-            char suit = suits[random.nextInt(suits.length)];
-            String rank = value[random.nextInt(value.length)];
-            hand[i] = suit + rank;
+            String card;
+            do {
+                char suit = suits[random.nextInt(suits.length)];
+                String value = rank[random.nextInt(rank.length)];
+                card = suit + value;
+            } while (dealtCards.contains(card));
+            hand[i] = card;
+            dealtCards.add(card);
         }
         return hand;
+    }
+
+    public static int calculateSum(String[] hand) {
+        int sum = 0;
+        for (String card : hand) {
+            sum += Integer.parseInt(card.substring(1));
+        }
+        return sum;
+    }
+
+    public static int countSpades(String[] hand) {
+        int count = 0;
+        for (String card : hand) {
+            if (card.charAt(0) == 'S') {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static boolean hasSpadeQueen(String[] hand) {
+        for (String card : hand) {
+            if (card.equals("S12")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isFlush(String[] hand) {
+        char suit = hand[0].charAt(0);
+        for (String card : hand) {
+            if (card.charAt(0) != suit) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/main/java/javafx.java
+++ src/main/java/javafx.java
@@ -1,16 +1,13 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-
-import javax.swing.*;
-import java.util.Stack;
-
 
 public class javafx extends Application {
+
+    private String[] hand;
 
     public static void main(String[] args) {
         launch(args);
@@ -18,16 +15,35 @@ public class javafx extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button("Click me!");
-        btn.setOnAction(event -> System.out.println("button clicked!"));
+        Button dealButton = new Button("Deal Hand");
+        Button checkButton = new Button("Check Hand");
+        Label handLabel = new Label();
+        Label sumLabel = new Label();
+        Label spadesCountLabel = new Label();
+        Label spadeQueenLabel = new Label();
+        Label flushLabel = new Label();
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        dealButton.setOnAction(event -> {
+            hand = DeckOfCards.dealHand(5);
+            handLabel.setText("Hand: " + java.util.Arrays.toString(hand));
+        });
 
-        Scene scene = new Scene(root, 400,400);
-        primaryStage.setTitle("ongodimbouttabust");
+        checkButton.setOnAction(event -> {
+            if (hand != null) {
+                sumLabel.setText("Sum of cards: " + DeckOfCards.calculateSum(hand));
+                spadesCountLabel.setText("Number of Spades: " + DeckOfCards.countSpades(hand));
+                spadeQueenLabel.setText("Has Spade Queen: " + DeckOfCards.hasSpadeQueen(hand));
+                flushLabel.setText("Is Flush: " + DeckOfCards.isFlush(hand));
+            } else {
+                sumLabel.setText("Please deal a hand first.");
+            }
+        });
+
+        VBox root = new VBox(10, dealButton, checkButton, handLabel, sumLabel, spadesCountLabel, spadeQueenLabel, flushLabel);
+        Scene scene = new Scene(root, 600, 400);
+
+        primaryStage.setTitle("Card Game");
         primaryStage.setScene(scene);
         primaryStage.show();
-
     }
 }
